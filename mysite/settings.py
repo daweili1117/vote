e099b26e5895fe510eacbb549d3b22c0d0e9642b
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2021-02-07 11:22:11 -0600

    2

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,20 +74,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tasks',
-        'USER': 'admin',
-        'PASSWORD': 'test1234'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tasks',
+#         'USER': 'admin',
+#         'PASSWORD': 'test1234'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
